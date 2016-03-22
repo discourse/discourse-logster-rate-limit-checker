693e078e79ef8e1a467c70d3f060cfc7fc8996d4
Guo Xiang Tan <tgx_world@hotmail.com>
2016-03-22 12:08:15 +0800

    First commit.

---
+++ plugin.rb
@@ -0,0 +1,43 @@
+# name: logster-rate-limit-checker
+# about: Add scheduled jobs to check the logster's rate limit.
+# version: 0.0.1
+# authors: Alan Tan (tgxworld)
+
+after_initialize do
+  if (RailsMultisite::ConnectionManagement.current_db == RailsMultisite::ConnectionManagement::DEFAULT) &&
+    module ::LogsterRateLimitChecker
+      STORE = Logster.store
+      RATE_LIMITS = STORE.instance_variable_get(:@rate_limits)[RailsMultisite::ConnectionManagement::DEFAULT]
+
+      if (RATE_LIMITS && !RATE_LIMITS.empty?)
+        def self.check_rate_limits(duration, limit)
+          RATE_LIMITS.each do |rate_limiter|
+            next if (duration != rate_limiter.duration) || !(callback = rate_limiter.callback)
+            rate = rate_limiter.retrieve_rate
+            callback.call(rate) if rate > limit
+          end
+        end
+
+        class PerMinuteChecker < Jobs::Scheduled
+          every 10.seconds
+
+          def execute(args)
+            ::LogsterRateLimitChecker.check_rate_limits(
+              60, SiteSetting.alert_admins_if_errors_per_minute
+            )
+          end
+        end
+
+        class PerHourChecker < Jobs::Scheduled
+          every 10.minute
+
+          def execute(args)
+            ::LogsterRateLimitChecker.check_rate_limits(
+              3600, SiteSetting.alert_admins_if_errors_per_hour
+            )
+          end
+        end
+      end
+    end
+  end
+end
